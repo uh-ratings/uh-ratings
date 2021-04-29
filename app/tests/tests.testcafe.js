52f52bff4e48c17ca28613d03ccc6034c4d7a58e
Clement <chowc89@hawaii.edu>
2021-04-29 12:20:51 -1000

    fix credentials username

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -8,7 +8,7 @@ import { adminPage } from './admin.Page';
 /* global fixture:false, test:false */
 
 /** Credentials for one of the sample users defined in settings.development.json. */
-const credentials = { username: 'admin@foo.com', password: 'changeme' };
+const credentials = { username: 'john@foo.com', password: 'changeme' };
 const credentials2 = { username: 'admin@foo.com', password: 'changeme' };
 
 fixture('meteor-application-template-react localhost test with default db')
