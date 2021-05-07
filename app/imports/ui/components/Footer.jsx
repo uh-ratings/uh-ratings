ce86e52b6f73cff93e0bf9b862493cfe3e31b688
Taylor Wong <taylorsw@hawaii.edu>
2021-05-07 13:05:06 -1000

    Issue #36

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, Image } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
@@ -7,11 +8,25 @@ class Footer extends React.Component {
     return (
       <footer>
         <div style={divStyle} className="ui center aligned container">
-          <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822 <br />
-          <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
+          <hr/>
+          <Grid container columns={3}>
+            <Grid.Column>
+              <a href="https://github.com/uh-ratings"> <h3> Contact Us </h3></a><br/>
+              UH-Ratings team and members: <br/>
+              <a href="https://chowc89.github.io/"> Clement </a> <br/>
+              <a href="https://hangbozhang.github.io/"> Hangbo </a> <br/>
+              <a href="https://nicoelee123.github.io/"> Nicholas </a> <br/>
+              <a href="https://rabitz808.github.io/"> Rafael </a> <br/>
+              <a href="https://taylorwwong.github.io/"> Taylor </a> <br/>
+            </Grid.Column>
+            <Grid.Column>
+              <Image size='small' src='images/UHratings.png' spaced='left'/>
+            </Grid.Column>
+            <Grid.Column>
+              <a href="https://github.com/uh-ratings/uh-ratings"> <h3>Source Code</h3> </a><br/>
+              Application built using the help of UH ICS meteor template <br/>
+            </Grid.Column>
+          </Grid>
         </div>
       </footer>
     );
